14fe8c814c5b178c6e9c3e49cd1f240604d2abb4
qashqyn <qashqyn291@gmail.com>
2022-05-22 16:29:42 +0600

    Study options

--- client/src/components/Study/Options/Options.js
+++ client/src/components/Study/Options/Options.js
@@ -1,17 +1,29 @@
 import React from "react";
-import { Card, Col, Container, Image, Row } from "react-bootstrap";
+import { Col, Container, Image, Row } from "react-bootstrap";
 
 import './styles.scss';
-import { LinkContainer } from "react-router-bootstrap";
+
+import OptionsImg from '../../../images/study-options.png';
+import StudyWithUsImg from '../../../images/StudyWithUs.png'
 
 
 const StudyOptions = () => {
     return (
         <div id="study-options">
             <Container>
-                <Image />
-                <Row xs={1} lg={2}>
+                <div id="options-top-img">
+                    <Image src={OptionsImg} />
+                    <h1>STUDY OPTIONS</h1>
+                </div>
+                <Row xs={1} lg={2} className="mb-5">
                     <Col>
+                        <Image src={StudyWithUsImg} />
+                    </Col>
+                    <Col>
+                        <h3>Study  in Foreign contries with academic mobility</h3>
+                        <p>
+                            Academic mobility is important for personal development, it fosters respect for diversity and enables cultural enrichment.  Within the framework of the program of academic mobility.
+                        </p>
                     </Col>
                 </Row>
             </Container>
